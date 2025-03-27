42970bccc16096a57e383c96161031ccb9fcef5f
kushagra <kushagra3@gmail.com>
2025-03-27 10:43:08 +0530

    database schema commit

---
+++ backend/src/models/usermodel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlenght: 6,
+     },
+    profilePic: {
+        type: String,
+        default: "",
+    }},
+      {timestamps: true},
+    )
+
+ const User = mongoose.model("User", userSchema)//always remenber module name will be singular and 1st letter capital
+ export default User
+// ----IMPP GENERAL------------------------
+//  for user detail consist of 
+//  Email 
+//  full name
+//  pswd
+//  profile pic
+// and id will be autogiven by it so no need to worry about that
+
+
