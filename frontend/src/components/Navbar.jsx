d6ddc3cf0d8009ea93aefcc1879ed273cb8dc796
kushagra <kushagramalviya3@gmail.com>
2025-04-11 22:09:00 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,6 +25,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2">
             <Link
               to={"/settings"}
+              
               className={`
               btn btn-sm gap-2 transition-colors
               
